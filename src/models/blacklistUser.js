c1f16451078e3933bd173c21f5e717ff50a56b42
lightny <132174688+lightny@users.noreply.github.com>
2024-05-18 13:31:29 -0400

    Delete src directory

--- src/models/blacklistUser.js
+++
@@ -1,8 +0,0 @@
-const { model, Schema } = require('mongoose');
-
-let blacklistUser = new Schema({
-    User: String,
-    Reason: String
-});
-
-module.exports = model('blacklistUser', blacklistUser);
